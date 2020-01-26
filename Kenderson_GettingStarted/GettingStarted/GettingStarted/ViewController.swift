814469990101fa3b7d16ba22a644f10ad62cf56a
KeydiKenderson <60267139+KeydiKenderson@users.noreply.github.com>
2020-01-25 21:15:57 -0700

    Add files via upload

---
+++ Kenderson_GettingStarted/GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Kenderson,Keydi Kay on 1/24/20.
+//  Copyright © 2020 Kenderson,Keydi Kay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+        //names.removeFirst()
+        
+        navigationController?.title = "Debugging"
+        //let x = 4
+        
+    }
+
+
+}
+
